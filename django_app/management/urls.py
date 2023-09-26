18262d63fcf6dedfd4e350cb0b72699cad1db93a
DGMEFG <97904453+DGMEFG@users.noreply.github.com>
2023-09-26 21:12:14 +0800

    Update urls.py

--- django_app/management/urls.py
+++ django_app/management/urls.py
@@ -18,9 +18,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='index'),
-    path('login/', views.user_login, name='login'),
-    # path('register/', views.register, name='register'),
+    path('home/api/login/', views.user_login, name='login'),
+    path('home/api/pic/', views.pic_solve, name='pic'),
+    path('home/', views.home_view, name='home'),
     path('api/send_verification_code/', views.send_verification_code, name='send_verification_code'),
 
 ]
